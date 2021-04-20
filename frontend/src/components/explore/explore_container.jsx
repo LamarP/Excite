8092009e860e95648001246ea5ae76f5c4e1196d
terryt925 <thindterry@yahoo.com>
2021-04-20 09:34:57 -0700

    Add explore files

---
+++ frontend/src/components/explore/explore_container.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import Explore from './explore';
+import {fetchAllExcites} from './excite_actions';
+
+const mSTP = (state) => {
+  return {
+    excites: Object.values(state.entities.excites)
+  }
+}
+
+const mDTP = (state) => {
+  fetchAllExcites: () => dispatch(fetchAllExcites()),
+  addExcite: (excite) => dispatch(addExcite(excite))
+}
+
+export default connect(mSTP, mDTP)(Explore);
