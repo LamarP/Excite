9b825ea5f40f467899730833e6ca009a3a0bc906
terryt925 <thindterry@yahoo.com>
2021-05-06 17:52:30 -0700

    Add excite titles

--- frontend/src/components/explore/excite_index_item.jsx
+++ frontend/src/components/explore/excite_index_item.jsx
@@ -18,12 +18,13 @@ export default class ExciteIndexItem extends React.Component {
     }
     render() {
         if(!this.props.scene) return null;
+        if(!this.props.excite) return null;
         // console.log(this.state.excite)
         return(
             <li className="scene-index-item" > 
                 <Link to={`/explore/${this.props.idx}`}>
                     <img src={this.props.scene.image} width='319' height='240' alt=""/>
-                    <div className='excite-index-item-title' >Title</div>
+                    <div className='excite-index-item-title' >{this.props.excite.title}</div>
                 </Link>
             </li>
         )
