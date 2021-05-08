c7aaca4641fca27115128464073f3005d4c956c1
frmendez21 <frmendez209@gmail.com>
2021-05-08 15:13:11 -0700

    added create excite backend functionality

--- controllers/excites.js
+++ controllers/excites.js
@@ -10,11 +10,19 @@ const ExcitesController = {
         return result;
     }, 
     createExcite: async payload => {
-        const excite = new Excite(payload)
-        const result = await excite.save()
+        const excite = new Excite({
+            title: payload.title, 
+            sceneId: payload.sceneId, 
+            sceneUrl: payload.sceneUrl, 
+            sceneTitle: payload.sceneTitle, 
+            sceneImage: payload.sceneImage, 
+            users: [payload.users], 
+            sounds: [payload.sounds]
+        });
+        const result = await excite.save();
         return result;
     }
-}
+};
 
 module.exports = ExcitesController;
 
