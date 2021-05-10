6c42d74700abea74ebfae0620aaf9ae1a99de792
frmendez21 <frmendez209@gmail.com>
2021-05-10 13:16:26 -0700

    added frontend and backend delete and create excite functionality

--- frontend/src/components/scenes/scene_index_item.jsx
+++ frontend/src/components/scenes/scene_index_item.jsx
@@ -5,7 +5,6 @@ class SceneIndexItem extends React.Component {
 
 
   render() {
-    console.log(this.props.scene)
     return(
       <div className="scene-index-item">
         <Link to={`/scenes/${this.props.sceneId}`}>
@@ -13,9 +12,6 @@ class SceneIndexItem extends React.Component {
             <img src={this.props.scene.image} width='319' height='240' alt=""/>
           </figure>
         </Link>
-        {/* <video width="320" height="240"  
-        src={this.props.scene.video_files.filter(vid => ( vid.quality === "hd"))[0].link} 
-        autoPlay muted loop className='vid' type='video/mp4'></video> */}
       </div>
     )
   }
