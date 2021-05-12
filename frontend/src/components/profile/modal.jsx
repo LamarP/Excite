1aa0cc69dc0a51e0af8d6aa6fce0028e6bba0fa3
terryt925 <thindterry@yahoo.com>
2021-05-12 11:09:41 -0700

    Style modal

--- frontend/src/components/profile/modal.jsx
+++ frontend/src/components/profile/modal.jsx
@@ -28,13 +28,14 @@ class Modal extends React.Component {
         ))
         let excitesList = excites.map((excite, idx) => (
             <li key={idx} onClick={() => this.handleSubmit(excite._id)} className="modal-excite-list-item">
-                <img src={excite.sceneImage} alt="" width="50" height="45"/>
+                <img src={excite.sceneImage} alt="" width="90" height="60"/>
                 <p className="modal-list-title">{excite.title}</p>
             </li>
         ))
         return(
            
                 <div className='modal-child' onClick={e => e.stopPropagation()}>
+                        <div className='modal-excite-list-title'><h1>Excites</h1></div>
                         <ul className="modal-excite-list-container">
                             {excitesList}
                         </ul>
