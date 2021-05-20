4e904fbe4915c14d62b3934331c70106cf0703d7
terryt925 <thindterry@yahoo.com>
2021-05-19 18:04:32 -0700

    Update app

--- frontend/src/components/landing/landing.jsx
+++ frontend/src/components/landing/landing.jsx
@@ -11,7 +11,7 @@ class Landing extends React.Component {
         <video 
             // src="https://player.vimeo.com/external/368320203.hd.mp4?s=ed62dd31538d7ef092250598fdb13c3cd0857812&profile_id=172&oauth2_token_id=57447761"
             src='https://player.vimeo.com/external/368320203.sd.mp4?s=38b1bac5d627b94fb902597643461ee5f233d00a&profile_id=165&oauth2_token_id=57447761'
-            preload autoPlay muted loop className='session-vid' type='video/mp4'>
+            autoPlay muted loop className='session-vid' type='video/mp4'>
         </video>
         <Link to={'/login'} style={{ textDecoration: 'none' }}>
             <h1  className='landing-title' >Excite</h1>
