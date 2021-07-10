29a4f6a0b834715473fa28520b9d7a4e59666e20
frmendez21 <frmendez209@gmail.com>
2021-07-09 18:05:33 -0700

    refactoring components ;

--- frontend/src/components/profile/modal.jsx
+++ frontend/src/components/profile/modal.jsx
@@ -1,48 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+const Modal = props => {
+    const {fetchExcites} = props;
 
-class Modal extends React.Component {
+    useEffect(() => {
+        fetchExcites();
+    }, [fetchExcites]);
 
-    handleSubmit(exciteId) {
+    const handleSubmit = (exciteId) => {
         const goal = {
-            goalId: this.props.goal._id,
-            title: this.props.goal.title, 
-            userId: this.props.user.id, 
+            goalId: props.goal._id,
+            title: props.goal.title, 
+            userId: props.user.id, 
             excites: exciteId
         };
-        this.props.processForm(goal)
+        props.processForm(goal)
             .then(() => {
-                this.props.toggleForm()
-                this.props.reRender('add', exciteId)
+                props.toggleForm()
+                props.reRender('add', exciteId)
                 return false;
-            })
+            });
     };
 
-    componentDidMount() {
-        this.props.fetchExcites()
-    };
+    if(!props.excites) return null;
 
-    render() {
-        if(!this.props.excites) return null;
-        let excites = this.props.excites.filter(excite => (
-            !this.props.goal.excites.includes(excite._id)
-        ))
-        let excitesList = excites.map((excite, idx) => (
-            <li key={idx} onClick={() => this.handleSubmit(excite._id)} className="modal-excite-list-item">
-                <img src={excite.sceneImage} alt="" width="90" height="60"/>
-                <p className="modal-list-title">{excite.title}</p>
-            </li>
-        ))
-        return(
-           
-                <div className='modal-child' onClick={e => e.stopPropagation()}>
-                        <div className='modal-excite-list-title'><h1>Excites</h1></div>
-                        <ul className="modal-excite-list-container">
-                            {excitesList}
-                        </ul>
-                </div>
-       
-        );
-    };
+    let excites = props.excites.filter(excite => (
+        !props.goal.excites.includes(excite._id)
+    ));
+
+    let excitesList = excites.map((excite, idx) => (
+        <li key={idx} onClick={() => handleSubmit(excite._id)} className="modal-excite-list-item">
+            <img src={excite.sceneImage} alt="" width="90" height="60"/>
+            <p className="modal-list-title">{excite.title}</p>
+        </li>
+    ));
+
+    return(
+        <div className='modal-child' onClick={e => e.stopPropagation()}>
+                <div className='modal-excite-list-title'><h1>Excites</h1></div>
+                <ul className="modal-excite-list-container">
+                    {excitesList}
+                </ul>
+        </div>
+    );
 };
 
 export default Modal;
--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -1,90 +1,150 @@
 
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { useState, useEffect, useCallback } from 'react';
 import GoalIndexItem from './goal_index_item';
 import GoalTitleModal from './goal_title_modal';
 import ExciteModal from './excite_modal';
 
+const Profile = props => {
+    const [showGoalForm, setGoalForm] = useState(false);
+    const [showExciteForm, setExciteForm] = useState(false);
 
-class Profile extends React.Component {
-    constructor(props) {
-        super(props)
-        this.goalFormToggle = this.goalFormToggle.bind(this);
-        this.exciteFormToggle = this.exciteFormToggle.bind(this);
-        this.renderProfile = this.renderProfile.bind(this);
-        this.state = { showGoalForm: false, showExciteForm: false, openModal: false}
-    }
-    
-    goalFormToggle() {
-        if(!this.state.showGoalForm) {
-            this.setState({showGoalForm:true, showExciteForm: false})
-        } else {
-            this.setState({showGoalForm: false})
-        }
-    };
-    exciteFormToggle() {
-        if(!this.state.showExciteForm) {
-            this.setState({showExciteForm:true, showGoalForm: false})
-        } else {
-            this.setState({showExciteForm: false})
-        }
-    }
+    const {fetchUserGoals} = props;
+    const id = props.user.id;
 
-    componentDidMount() {
-        this.renderProfile()
-    }
+    const memoCallBack = useCallback(() => {
+        const renderProfile = id => fetchUserGoals(id);
+        renderProfile(id);
 
-    renderProfile() {
-        this.props.fetchUserGoals(this.props.user.id)
-    }
-    
-    render() {
-        let createModal = this.state.showGoalForm ? 
-        <div className='modal-background' onClick={this.goalFormToggle}>
-              <GoalTitleModal toggleForm={this.goalFormToggle} fetch={this.props.fetchUserGoals}/>
-        </div> : <div></div>;
+    }, [fetchUserGoals, id]);
 
-        let exciteModal = this.state.showExciteForm ? 
-        <div className='modal-background' onClick={this.exciteFormToggle}>
+    useEffect(() => {
+        memoCallBack()
+    }, [memoCallBack]);
+
+    const createModal =   
+        <div className='modal-background' onClick={() => showGoalForm ? setGoalForm(false) : setGoalForm(true) }>
+              <GoalTitleModal toggleForm={() => showGoalForm ? setGoalForm(false) : setGoalForm(true)} fetch={props.fetchUserGoals}/>
+        </div>;
+
+    const exciteModal = 
+        <div className='modal-background' onClick={() => showExciteForm ? setExciteForm(false) : setExciteForm(true) }>
               <ExciteModal />
-        </div>
-            : 
-        <div></div>;
+        </div>;
 
-        if(!this.props.goals.data) return null;
-        const goals = this.props.goals.data.map((goal, idx) => (
+    if(!props.goals.data) return null;
+
+    const goals = props.goals.data.map((goal, idx) => (
             <GoalIndexItem key={idx} 
                 goal={goal} 
-                fetchExcite={this.props.fetchExcite} 
-                user={this.props.user} 
-                removeExcite={this.props.removeExcite}
-                deleteGoal={this.props.deleteGoal}
-                updateGoal={this.props.updateGoal}
-                renderProfile={this.renderProfile}
+                fetchExcite={props.fetchExcite} 
+                user={props.user} 
+                removeExcite={props.removeExcite}
+                deleteGoal={props.deleteGoal}
+                updateGoal={props.updateGoal}
+                renderProfile={memoCallBack}
             />
         ));
-        return (
-            <div className='profile-page'>
-                <div className="goal-create-container">
-                    <div className='create-div'>
-                        <button className="goal-create-button" onClick={this.goalFormToggle}>+Create Goal</button>
-                        <button className="excite-create-button" onClick={this.exciteFormToggle}>+Create Excite</button>
-                    </div>
 
-                    {createModal}
-                    {exciteModal}
-                    
+    return (
+        <div className='profile-page'>
+            <div className="goal-create-container">
+                <div className='create-div'>
+                    <button className="goal-create-button" onClick={() => showGoalForm ? setGoalForm(false) : setGoalForm(true) }>+Create Goal</button>
+                    <button className="excite-create-button" onClick={() => showExciteForm ? setExciteForm(false) : setExciteForm(true)}>+Create Excite</button>
                 </div>
-
-                    <div className='goal-grid-parent'>
-                        {goals}
-                    </div>
-                
-                {/* {createModal} */}
+                {showGoalForm && createModal}
+                {showExciteForm && exciteModal}
                 
             </div>
-        );
-    }
-}
+                <div className='goal-grid-parent'>
+                    {goals}
+                </div>
+            {/* {createModal} */}
+        </div>
+    );
+    
+};
 
-export default withRouter(Profile);
+export default Profile;
+// class Profile extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.goalFormToggle = this.goalFormToggle.bind(this);
+//         this.exciteFormToggle = this.exciteFormToggle.bind(this);
+//         this.renderProfile = this.renderProfile.bind(this);
+//         this.state = { showGoalForm: false, showExciteForm: false, openModal: false}
+//     }
+    
+//     goalFormToggle() {
+//         if(!this.state.showGoalForm) {
+//             this.setState({showGoalForm:true, showExciteForm: false})
+//         } else {
+//             this.setState({showGoalForm: false})
+//         }
+//     };
+//     exciteFormToggle() {
+//         if(!this.state.showExciteForm) {
+//             this.setState({showExciteForm:true, showGoalForm: false})
+//         } else {
+//             this.setState({showExciteForm: false})
+//         }
+//     }
+
+//     componentDidMount() {
+//         this.renderProfile()
+//     }
+
+//     renderProfile() {
+//         this.props.fetchUserGoals(this.props.user.id)
+//     }
+    
+//     render() {
+//         let createModal = this.state.showGoalForm ? 
+//         <div className='modal-background' onClick={this.goalFormToggle}>
+//               <GoalTitleModal toggleForm={this.goalFormToggle} fetch={this.props.fetchUserGoals}/>
+//         </div> : <div></div>;
+
+//         let exciteModal = this.state.showExciteForm ? 
+//         <div className='modal-background' onClick={this.exciteFormToggle}>
+//               <ExciteModal />
+//         </div>
+//             : 
+//         <div></div>;
+
+//         if(!this.props.goals.data) return null;
+//         const goals = this.props.goals.data.map((goal, idx) => (
+//             <GoalIndexItem key={idx} 
+//                 goal={goal} 
+//                 fetchExcite={this.props.fetchExcite} 
+//                 user={this.props.user} 
+//                 removeExcite={this.props.removeExcite}
+//                 deleteGoal={this.props.deleteGoal}
+//                 updateGoal={this.props.updateGoal}
+//                 renderProfile={this.renderProfile}
+//             />
+//         ));
+//         return (
+//             <div className='profile-page'>
+//                 <div className="goal-create-container">
+//                     <div className='create-div'>
+//                         <button className="goal-create-button" onClick={this.goalFormToggle}>+Create Goal</button>
+//                         <button className="excite-create-button" onClick={this.exciteFormToggle}>+Create Excite</button>
+//                     </div>
+
+//                     {createModal}
+//                     {exciteModal}
+                    
+//                 </div>
+
+//                     <div className='goal-grid-parent'>
+//                         {goals}
+//                     </div>
+                
+//                 {/* {createModal} */}
+                
+//             </div>
+//         );
+//     }
+// }
+
+// export default withRouter(Profile);
